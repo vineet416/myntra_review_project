d7ec14c3d075f0172c89bc3b78b10e72310ae927
vineet416 <vineetpatel468@gmail.com>
2024-11-26 12:14:34 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='Vineet',
+    author_email='vineetpatel416@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
